32eb59affa7f594491d6df85076f2b2ad69bdc05
byfranco777 <mainaccjrc@gmail.com>
2023-06-23 15:27:52 +0200

    web porfavor

--- wp-content/uploads/uag-plugin/assets/0/uag-js-47-1687523622.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBForms.init( {"block_id":"766a7eec","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"mainaccjrc@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"alguien me ha contactado","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Por favor, completa el captcha de arriba.","confirmationUrl":""}, '.uagb-block-766a7eec', 47 );
-});
- });
